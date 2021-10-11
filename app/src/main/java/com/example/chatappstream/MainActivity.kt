d5f78bf054f0b9d4a55ec26c02be0f47efbe90b4
mohammad <m.zandieh1999@gmail.com>
2021-10-11 23:21:42 +0330

    add part `1`

--- app/src/main/java/com/example/chatappstream/MainActivity.kt
+++ app/src/main/java/com/example/chatappstream/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.chatappstream
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.chatappstream.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
