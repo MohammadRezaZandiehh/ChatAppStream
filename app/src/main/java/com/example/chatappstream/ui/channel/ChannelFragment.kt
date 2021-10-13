c36fa0ab0d48c538786ab63e71e12e569d042b89
mohammad <m.zandieh1999@gmail.com>
2021-10-13 16:56:10 +0330

    add part 3 and all target

--- app/src/main/java/com/example/chatappstream/ui/channel/ChannelFragment.kt
+++ app/src/main/java/com/example/chatappstream/ui/channel/ChannelFragment.kt
@@ -3,11 +3,15 @@ package com.example.chatappstream.ui.channel
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.viewbinding.ViewBinding
+import com.example.chatappstream.R
 import com.example.chatappstream.databinding.FragmentChannelBinding
+import com.example.chatappstream.util.navigateSafely
 import com.plcoding.streamchatapp.ui.BindingFragment
 import dagger.hilt.android.AndroidEntryPoint
 import io.getstream.chat.android.client.models.Filters
@@ -16,6 +20,7 @@ import io.getstream.chat.android.ui.channel.list.header.viewmodel.bindView
 import io.getstream.chat.android.ui.channel.list.viewmodel.ChannelListViewModel
 import io.getstream.chat.android.ui.channel.list.viewmodel.bindView
 import io.getstream.chat.android.ui.channel.list.viewmodel.factory.ChannelListViewModelFactory
+import kotlinx.coroutines.flow.collect
 
 @AndroidEntryPoint                                                                                  //access to inject dependency --> in Android Component some Activity and Fragment
 class ChannelFragment:BindingFragment<FragmentChannelBinding>() {
@@ -23,7 +28,7 @@ class ChannelFragment:BindingFragment<FragmentChannelBinding>() {
     override val bindingInflater: (LayoutInflater) -> ViewBinding
         get() = FragmentChannelBinding::inflate
 
-    private val viewModel: ChannelViewModel by activityViewModels()                                 // when we clicked on createButton in dialogFragment , dialog is destroyed . but the viewModel actually not destroyed , because it's bound to the activity (min:8)
+    private val viewModel: ChannelViewModel by activityViewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -51,6 +56,40 @@ class ChannelFragment:BindingFragment<FragmentChannelBinding>() {
         binding.channelListHeaderView.setOnUserAvatarClickListener {
             viewModel.logOut()
             findNavController().popBackStack()
+        }
+
+        binding.channelListHeaderView.setOnActionButtonClickListener {
+            findNavController().navigateSafely(
+                R.id.action_channelFragment_to_createChannelDialog
+            )
+        }
+
+        binding.channelListView.setChannelItemClickListener { channel ->
+            findNavController().navigateSafely(
+                R.id.action_channelFragment_to_chatFragment,
+                Bundle().apply { putString("channelId", channel.cid) }
+            )
+        }
+
+        lifecycleScope.launchWhenStarted {
+            viewModel.createChannelEvent.collect { event ->
+                when(event) {
+                    is ChannelViewModel.CreateChannelEvent.Error -> {
+                        Toast.makeText(
+                            requireContext(),
+                            event.error,
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
+                    is ChannelViewModel.CreateChannelEvent.Success -> {
+                        Toast.makeText(
+                            requireContext(),
+                            R.string.channel_created,
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
+                }
+            }
         }
     }
 }
--- app/src/main/java/com/example/chatappstream/ui/channel/ChannelViewModel.kt
+++ app/src/main/java/com/example/chatappstream/ui/channel/ChannelViewModel.kt
@@ -1,21 +1,63 @@
 package com.example.chatappstream.ui.channel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.chatappstream.ui.login.LoginViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import io.getstream.chat.android.client.ChatClient
+import io.getstream.chat.android.client.call.await
 import io.getstream.chat.android.client.models.User
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.launch
+import java.util.*
 import javax.inject.Inject
 
 @HiltViewModel
 class ChannelViewModel @Inject constructor(
-    private var client: ChatClient
-) : ViewModel() {
+    private val client: ChatClient
+): ViewModel() {
 
-    fun logOut() {
+    private val _createChannelEvent = MutableSharedFlow<CreateChannelEvent>()
+    val createChannelEvent = _createChannelEvent.asSharedFlow()
+
+    fun createChannel(channelName: String) {
+        val trimmedChannelName = channelName.trim()
+        viewModelScope.launch {
+            if(trimmedChannelName.isEmpty()) {
+                _createChannelEvent.emit(CreateChannelEvent.Error("The channel name can't be empty."))
+                return@launch
+            }
+            val result = client.channel(
+                channelType = "messaging",
+                channelId = UUID.randomUUID().toString()
+            ).create(
+                mapOf(
+                    "name" to trimmedChannelName,
+                )
+            ).await()
+            if(result.isError) {
+                _createChannelEvent.emit(
+                    CreateChannelEvent.Error(result.error().message ?: "Unknown error")
+                )
+                return@launch
+            }
+            _createChannelEvent.emit(CreateChannelEvent.Success)
+        }
+    }
+
+    fun logOut() {                                                                                  //if in channelFragment we clicked on channelListHeaderView (flash that go back to loginFragment )
         client.disconnect()
     }
 
+
     fun getUser(): User? {                                                                          //access to current user
         return client.getCurrentUser()
+    }
+
+
+    sealed class CreateChannelEvent {
+        data class Error(val error: String) : CreateChannelEvent()
+        object Success : CreateChannelEvent()
     }
 }
